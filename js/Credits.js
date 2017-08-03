87ba0f4926dd68c5f8ba1f17674e6a16f902bc4b
Thijs de Witt <thijs757@gmail.com>
2017-08-03 15:33:22 +0200

    Updated Credits.js

--- js/Credits.js
+++ js/Credits.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-console.log(
-    '-----------------------------------'   + '\n' +
-    '     Official VertexOS Website'        + '\n' +
-    '-----------------------------------'   + '\n' + '\n' +
-    'Website designed by: Thijs de Witt'    + '\n' +
-    'Website made by: Thijs de Witt'        + '\n' +
-    'Images designed by: Alex B');
+$(document).ready(function () {
+    console.log(
+        '-----------------------------------' + '\n' +
+        '     Official VertexOS Website     ' + '\n' +
+        '-----------------------------------' + '\n' + '\n' +
+        'Website designed by: Thijs de Witt ' + '\n' +
+        'Website made by: Thijs de Witt     ' + '\n' +
+        'Images designed by: Alex B         ');
 });

86f25cce84e4a852444b41583b46187f204d1441
Thijs de Witt <thijs757@gmail.com>
2017-08-03 21:33:01 +0200

    ripple: added ripple-ease-out animation

--- js/RippleEffect.js
+++ js/RippleEffect.js
@@ -1,37 +1,16 @@
 (function (window, $) {
 
     $(function () {
+        // Check if 
         $('.button a, .ripple').on('mousedown', function (event) {
 
-            // Set background color
-            $(this).css({
-                background: 'rgba(0, 0, 0, 0.06)'
-            });
-
-            // Add class ripple-overlay
-            $(this).addClass('ripple-overlay');
-        });
-
-        $('.button a, .ripple').on('mouseup', function (event) {
-
-            // Set background back to transparent
-            $(this).css({
-                background: 'transparent'
-            });
-
-            // Remove class ripple-overlay after (1000) ms
-            setTimeout(function () {
-                $('.ripple-overlay').removeClass('ripple-overlay');
-            }, 1000);
-        });
-        //-----------------------------------------------------------
-        $('.button a, .ripple').on('click', function (event) {
-
+            // Just your basics variables
             var $div = $('<div/>'),
                 btnOffset = $(this).offset(),
                 xPos = event.pageX - btnOffset.left,
                 yPos = event.pageY - btnOffset.top;
 
+            // Add class ripple-effect to the div
             $div.addClass('ripple-effect');
             var $ripple = $(".ripple-effect");
 
@@ -40,20 +19,27 @@
                 $(".ripple-effect:first").remove();
             };
 
+            // Set needed style properties 
             $div
                 .css({
                     top: yPos - (50 / 2),
                     left: xPos - (50 / 2),
                     background: $(this).data("ripple-color")
                 })
-                .appendTo($(this));
 
-            // Remove ripple effect after 1000 milliseconds
-            window.setTimeout(function () {
-                $div.remove();
-            }, 1000);
+                //Place div under the element
+                .appendTo($(this));
         });
 
+        $('.button a, .ripple, .ripple-effect').on('mouseup mouseout', function (event) {
+
+            var $ripple = $(".ripple-effect");
+            $ripple.delay(300).fadeOut(300);
+
+            setTimeout(function () {
+                $ripple.remove();
+            }, 1000);
+        });
     });
 
 })(window, jQuery);

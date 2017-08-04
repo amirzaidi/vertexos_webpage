f44f25990bf455e45638adb088a0ef954b158801
Thijs de Witt <thijs757@gmail.com>
2017-08-04 20:03:27 +0200

    ripple: redone ripple effect from scratch

--- js/RippleEffect.js
+++ js/RippleEffect.js
@@ -1,10 +1,40 @@
 (function (window, $) {
 
     $(function () {
-        // Check if 
-        $('.button a, .ripple').on('mousedown', function (event) {
 
-            // Just your basics variables
+
+        var $btn = $('.ripple, .button a');
+        $btn.on('pointerdown', function (e) {
+
+            var $div = $("<div/>"),
+                $btn = $('.ripple'),
+                $ripple = $(".ripple-ease-in");
+            //             Add class ripple - effect to the div
+            $div.addClass('ripple-ease-in');
+
+            // Makes sure that the ripple effect wont get too dark caused by too many effect overlapping
+            if ($ripple.length >= 3) {
+                $(".ripple-ease-in:first").remove();
+            };
+
+            // Set needed style properties 
+            $div
+                .css({
+                    background: $(this).data("ripple-color")
+                })
+
+                //Place div under the element
+                .appendTo($(this));
+            console.log('test');
+        }); // Ripple-Ease-In Animation
+        $btn.on('mouseup', function (e) {
+            var $ripple1 = $(".ripple-ease-in");
+            //fadeOut 
+            $ripple1.delay(300).fadeOut(300);
+            setTimeout(function () {
+                $ripple1.remove();
+            }, 1000);
+
             var $div = $('<div/>'),
                 btnOffset = $(this).offset(),
                 xPos = event.pageX - btnOffset.left,
@@ -29,17 +59,19 @@
 
                 //Place div under the element
                 .appendTo($(this));
-        });
-
-        $('.button a, .ripple, .ripple-effect').on('mouseup mouseout', function (event) {
-
-            var $ripple = $(".ripple-effect");
-            $ripple.delay(300).fadeOut(300);
 
             setTimeout(function () {
-                $ripple.remove();
+                $div.remove();
             }, 1000);
-        });
+
+        }); // Ripple Grow Animation
+        $(document).on('scroll touchend mouseup', function () {
+            var $ripple1 = $(".ripple-ease-in");
+            $ripple1.delay(300).fadeOut(300);
+            setTimeout(function () {
+                $ripple1.remove();
+            }, 1000);
+        }); //Remove divs. afer scroll or touchend
     });
 
 })(window, jQuery);
